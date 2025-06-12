b508feb8099f4c95f1932ea4d5f6a56f6856635c
Kalakai Zsolt <kalakaizsolt@gmail.com>
2025-06-12 09:01:04 +0200

    alapprojekt (első commit)

---
+++ src/main/java/com/example/blogom/BlogomApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogom/BlogomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
